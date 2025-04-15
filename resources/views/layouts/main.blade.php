59b5db723ec9e139ae9020849461649db2a0553f
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2025-04-15 09:16:27 -0500

    addBotstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <!--Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!--Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+
+    <!-- Css APlicação -->
     <link rel="stylesheet" href="/css/styles.css">
     <script src="/js/scripts.js"></script>
 </head>
--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,2 +1,6 @@
+@extends('layouts.main')
+@section('title', 'Procutos')
+@section('content')
 <h1>Pagina de Produtos</h1>
 <a href="/">Retornar para Home</a>
+@endsection
