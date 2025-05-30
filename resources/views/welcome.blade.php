47d4aa97eea3ab133c14b2dd2439ef2b383f6912
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2025-05-30 13:55:50 -0500

    upload de imagem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,27 +3,27 @@
 @section('content')
     
 <div id="search-container" class="col-md-12">
-    <h1>Buscar evento</h1>
+    <h1>Busque um evento</h1>
     <form action="">
-        <input type="text" id="search" name="search" class="form-control" placeholder="Pesquise nome de um evento">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
 <div id="events-container" class="col-md-12">
     <h2>Próximos Eventos</h2>
-    <p>Veja os eventos dos poximos dias</p>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
     <div id="cards-container" class="row">
         @foreach($events as $event)
-        <div class="card-col-md-3">
-            <img src="/img/bannersa.jpeg" alt="{{$event->title}}">
+        <div class="card col-md-3">
+            <img src="/img/events/{{$event->image}}" alt="{{ $event->title }}">
             <div class="card-body">
-                <div class="card-date">10/09/2025</div>
-                <h5 class="card-title">{{$event->title}}</h5>
+                <p class="card-date">10/09/2020</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participants">X Participantes</p>
                 <a href="#" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
-    </div>
         @endforeach
+    </div>
 </div>
         
 @endsection
